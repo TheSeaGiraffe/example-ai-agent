0a1f2f2d304d71978b1a6e2f43055362da623b5c
Fahmi Adi Nugraha <fnugraha@gmail.com>
2025-07-03 09:53:04 +0700

    Update main function with a feedback loop that allows the AI agent to actually make decisions.

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ from google.genai.types import Content, Part
 import functions.function_declarations as funcdecs
 from functions.call_function import call_function
 
+MAX_AGENT_ITERATIONS = 20
+
 available_functions = types.Tool(
     function_declarations=[
         funcdecs.schema_get_files_info,
@@ -52,48 +54,71 @@ def main():
     system_prompt = """
     You are a helpful AI coding agent.
 
-    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+    When a user asks a question or makes a request, make a function call plan. You can
+    peform the following operations:
 
     - List files and directories
     - Read file contents
     - Execute Python files with optional arguments
     - Write or overwrite files
 
-    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    All of these operations are constrained to a working directory that has already been
+    provided by the calling code and, thus, does not need to be specified in any of the
+    function calls. All paths you provide should be relative to this working directory.
+    Also, any of the questions asked by the user should be answered within the context
+    of this working directory. When asked about code functionality, try looking through
+    any files with a '.py' extension in the current directory or any subdirectories.
+    When attempting to retrieve information about files always start by looking in the
+    current directory (i.e., the '.' directory).
     """
 
+    reached_max_iter = False
     messages = [Content(role="user", parts=[Part(text=user_prompt)])]
-    response = client.models.generate_content(
-        model=model,
-        contents=messages,
-        config=types.GenerateContentConfig(
-            tools=[available_functions], system_instruction=system_prompt
-        ),
-    )
+    for it in range(MAX_AGENT_ITERATIONS):
+        response = client.models.generate_content(
+            model=model,
+            contents=messages,
+            config=types.GenerateContentConfig(
+                tools=[available_functions], system_instruction=system_prompt
+            ),
+        )
+        if response.candidates is not None:
+            for candidate in response.candidates:
+                messages.append(candidate.content)
 
-    if args.verbose:
-        print(f"User prompt: {user_prompt}")
-        print(f"Prompt tokens: {verbose_token_count(user_prompt)}")
-        # Will need to rethink how to handle this. Comment out for now.
-        # print(
-        #     f"Response tokens: {verbose_token_count(response.text) if response.text else 0}\n"
-        # )
+        if args.verbose:
+            print(f"User prompt: {user_prompt}")
+            print(f"Prompt tokens: {verbose_token_count(user_prompt)}")
+            # Will need to rethink how to handle this. Comment out for now.
+            # print(
+            #     f"Response tokens: {verbose_token_count(response.text) if response.text else 0}\n"
+            # )
 
-    function_calls = response.function_calls
-    if function_calls:
-        for function_call in function_calls:
-            function_call_results = call_function(function_call, args.verbose)
-            function_call_results_response = function_call_results.parts[
-                0
-            ].function_response.response
-            if function_call_results_response is None:
-                raise Exception(
-                    f'Function call "{function_call.name}" did not return a response'
-                )
-            if args.verbose:
-                print(f"-> {function_call_results_response}")
-    else:
-        print(f"Response: {response.text}")
+        function_calls = response.function_calls
+        if function_calls:
+            for function_call in function_calls:
+                function_call_results = call_function(function_call, args.verbose)
+                function_call_results_response = function_call_results.parts[
+                    0
+                ].function_response.response
+                if function_call_results_response is None:
+                    raise Exception(
+                        f'Function call "{function_call.name}" did not return a response'
+                    )
+                messages.append(function_call_results)
+                if args.verbose:
+                    print(f"-> {function_call_results_response}")
+
+        else:
+            print(f"Response: {response.text}")
+            break
+
+        reached_max_iter = (it + 1) == MAX_AGENT_ITERATIONS
+
+    if args.verbose and reached_max_iter:
+        print(
+            f"Agent reached the maximum number of iterations ({MAX_AGENT_ITERATIONS})"
+        )
 
 
 if __name__ == "__main__":
